e8e3bbd2e06294bf9807df5c671f7fb1137efcca
Kuncy <louis.samassa@gmail.com>
2025-07-07 20:32:42 +0200

    Added Laravel-Echo and pusher.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,20 @@ import router from './router'
 
 import './assets/main.css'
 
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'local',
+  wsHost: '127.0.0.1',
+  wsPort: 6001,
+  forceTLS: false,
+  disableStats: true,
+  enabledTransports: ['ws', 'wss'],
+});
+
 const app = createApp(App)
 
 app.use(createPinia())
