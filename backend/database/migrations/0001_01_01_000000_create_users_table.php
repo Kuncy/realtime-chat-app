a7a64b7cb0807f66ffc7a4be8c70081a8c4956fa
Kuncy <louis.samassa@gmail.com>
2025-07-07 19:59:21 +0200

    Edited the default Users table for the phone number use case.

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('username')->unique();
+            $table->string('phone_number')->unique();
+            $table->string('profile_picture')->nullable(); // URL or filename
+            $table->string('role')->default('user');
+            $table->string('password')->nullable(); // Optional if using OTP only
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('phone_number')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
